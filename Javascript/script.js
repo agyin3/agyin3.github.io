587eb0236157794b0a05fdccb50c20c8ec202b53
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-07-26 12:51:41 -0600

    change skills zindex; add javascript 2 hide skills

--- Javascript/script.js
+++ Javascript/script.js
@@ -16,18 +16,18 @@ const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
 const overlayMenu = document.getElementById("overlay");
 const footer = document.getElementById("footer");
-const contact = document.getElementById("contact-form");
+const skills = document.getElementById("skills");
 
 const openOnClick = () => {
     overlayMenu.classList.add("open");
     footer.classList.add("hidden");
-    contact.classList.add("hidden");
+    skills.classList.add("hidden");
 }
 
 const closeOnClick = () => {
     overlayMenu.classList.remove("open");
     footer.classList.remove("hidden");
-    contact.classList.remove("hidden");
+    skills.classList.remove("hidden");
 }
 
 openMenu.addEventListener("click", openOnClick);
